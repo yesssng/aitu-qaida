8bf158e48fd706aab90c2380d7f104f56d141b66
Adil <a.esengarin@a1qa.com>
2024-11-06 22:58:59 +0500

    commit

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
         <div className={welcomePage.welcome}>
             <div className={welcomePage.welcomeDesc}>
                 <h1>Build the path of your dreams</h1>
-                <p>A unique adventure awaits you, and it will begin with an amazing conversation with the Astana Tourist Bot</p>
+                <p>A unique adventure awaits you, and it will begin with an amazing conversation with the Astana Tourist Bot made by Adil Yessengarin, Bauyrzhan Zeinoldaev, Raiymbek Zhumanaliuly</p>
                 <div className={welcomePage.welcomeAction}>
                     <Button variant="contained"  style={{fontSize: '0.8rem', borderRadius: '0', backgroundColor: '#0065F2', width: 150}}>Start</Button>
                     <p>AI will help find the best solution for your comfort</p>
